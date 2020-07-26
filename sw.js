291b846c770c2747286501167654e437db13fb46
Arturo Rodríguez <red.cerberus640@gmail.com>
2020-07-26 00:08:13 -0500

    Modificaciones de iphone x+

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v4';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 // Corazón de la aplicación
@@ -40,6 +40,9 @@ self.addEventListener('activate', e => {
     const respuesta = caches.keys().then(keys => {
         keys.forEach(key => {
             if (key.includes('static') && key !== STATIC_CACHE) {
+                return caches.delete(key);
+            }
+            if (key.includes('dynamic') && key !== DYNAMIC_CACHE) {
                 return caches.delete(key);
             }
         });
